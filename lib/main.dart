1348585290f94f7ef07ab96f1f850a2176393ed7
developython14 <djmel1955@gmail.com>
2023-03-15 17:22:03 +0100

    initiliastion of the projects

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,35 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get_mac/get_mac.dart';
+import 'package:flutter_windowmanager/flutter_windowmanager.dart';
 
 void main() {
-  runApp(SplashScreen());
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: SplashScreen(),
+    );
+  }
 }
 
 class SplashScreen extends StatefulWidget {
@@ -23,22 +48,16 @@ class _SplashScreenState extends State<SplashScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: Scaffold(
-        backgroundColor: Colors.white,
-        body: Center(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(
-                "images/logo.png",
-                height: 250.0,
-                width: 250.0,
-              ),
-              Text(_platformID)
-            ],
-          ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('hackelbac prototype'),
+      ),
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [Text('unique data of devices Infos'), Text(_platformID)],
         ),
       ),
     );
@@ -56,5 +75,57 @@ class _SplashScreenState extends State<SplashScreen> {
     setState(() {
       _platformID = platformVersion;
     });
+  }
+}
+
+
+import 'package:flutter/material.dart';
+import 'package:flutter_windowmanager/flutter_windowmanager.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool _secureMode = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Plugin example app'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Text('Secure Mode: ${_secureMode.toString()}\n'),
+              ElevatedButton(
+                  onPressed: () async {
+                    final secureModeToggle = !_secureMode;
+
+                    if (secureModeToggle == true) {
+                      await FlutterWindowManager.addFlags(
+                          FlutterWindowManager.FLAG_SECURE);
+                    } else {
+                      await FlutterWindowManager.clearFlags(
+                          FlutterWindowManager.FLAG_SECURE);
+                    }
+
+                    setState(() {
+                      _secureMode = !_secureMode;
+                    });
+                  },
+                  child: const Text("Toggle Secure Mode")),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
