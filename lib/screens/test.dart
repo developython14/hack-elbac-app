3ca0e00abb2c0d97ed7c98fe0b45fac4742f58cf
developython14 <djmel1955@gmail.com>
2023-03-20 13:32:55 +0100

    work on detecting devices

--- lib/screens/test.dart
+++ lib/screens/test.dart
@@ -7,7 +7,6 @@
 import 'dart:async';
 import 'dart:developer' as developer;
 import 'dart:io';
-
 import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
